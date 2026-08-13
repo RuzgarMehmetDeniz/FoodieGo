fde9d04fdce634f00ddcc989be8f073eb3c65c9e
Rüzgar Mehmet Deniz <ruzgarmehmetdeniz@gmail.com>
2026-08-13 22:06:46 +0300

    Add project files.

---
+++ FoodieGo/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FoodieGo")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FoodieGo.Pages")]
